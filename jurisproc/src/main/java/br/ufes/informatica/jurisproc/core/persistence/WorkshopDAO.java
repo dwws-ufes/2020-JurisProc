145792a9f7575bcfded8c1e3b55e831af722330a
Pedro Hoppe <pedrohbh@github.com>
2021-04-13 19:41:21 -0300

    Adequações - parte 7

---
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/persistence/WorkshopDAO.java
@@ -0,0 +1,11 @@
+package br.ufes.informatica.jurisproc.core.persistence;
+
+import javax.ejb.Local;
+
+import br.ufes.inf.nemo.jbutler.ejb.persistence.BaseDAO;
+import br.ufes.informatica.jurisproc.core.domain.Workshop;
+
+@Local
+public interface WorkshopDAO extends BaseDAO<Workshop> {
+
+}
