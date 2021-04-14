aed823ff0ad4e6102411a3ea3c1fee12876488a1
Pedro Hoppe <pedrohbh@github.com>
2021-04-13 21:54:57 -0300

    Adicionado serviço de email

---
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/infra/MailSender.java
@@ -0,0 +1,32 @@
+package br.ufes.informatica.jurisproc.infra;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@ApplicationScoped
+public class MailSender
+{
+	@Resource(mappedName = "java:jboss/mail/mailtrap")
+	private Session session;
+	
+	public void send(String from, String to, String subject, String body)
+	{
+		Message mimeMessage = new MimeMessage(session);
+		try
+		{
+			mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			mimeMessage.setFrom(new InternetAddress(from));
+			mimeMessage.setSubject(subject);
+			mimeMessage.setContent(body, "text/html");
+		} catch (MessagingException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+}
