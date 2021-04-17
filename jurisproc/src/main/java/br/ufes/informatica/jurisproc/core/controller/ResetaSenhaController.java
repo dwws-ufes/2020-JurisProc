f393ac0c11dd3e9846ad10b88af59e040f52e62e
Pedro Hoppe <pedrohbh@github.com>
2021-04-16 22:46:17 -0300

    Refatoração de reseta senha - parte 4

--- jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/controller/ResetaSenhaController.java
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/controller/ResetaSenhaController.java
@@ -1,10 +1,16 @@
 package br.ufes.informatica.jurisproc.core.controller;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import org.jboss.security.Base64Encoder;
 
 import br.ufes.informatica.jurisproc.core.domain.Usuario;
 import br.ufes.informatica.jurisproc.core.persistence.UsuarioDAO;
@@ -37,7 +43,22 @@ public class ResetaSenhaController implements Serializable
 		}
 		
 		usuario.setLinkResetaSenha(null);
-		usuarioDAO.merge(usuario);		
+		
+		String senhaCriptografada = usuario.getSenha();
+		
+		MessageDigest messageDigest;
+		try
+		{
+			messageDigest = MessageDigest.getInstance("SHA-256");
+			byte []encoded = messageDigest.digest(senhaCriptografada.getBytes(StandardCharsets.UTF_8));
+			senhaCriptografada = Base64Encoder.encode(encoded);
+			usuario.setSenha(senhaCriptografada);
+			usuarioDAO.merge(usuario);		
+		} catch (NoSuchAlgorithmException | IOException e)
+		{
+			return "/users/login.xhtml?faces-redirect=true";
+		}
+		
 		return "/users/login.xhtml?faces-redirect=true";
 	}
 	
