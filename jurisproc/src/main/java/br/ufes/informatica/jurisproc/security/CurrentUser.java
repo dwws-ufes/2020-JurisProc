46a7081c18eb637da3c78d76e0215f66cd36d09a
Pedro Hoppe <pedrohbh@github.com>
2021-04-17 17:13:33 -0300

    Adição do logout

--- jurisproc/src/main/java/br/ufes/informatica/jurisproc/security/CurrentUser.java
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/security/CurrentUser.java
@@ -5,9 +5,11 @@ import java.security.Principal;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import br.ufes.informatica.jurisproc.core.domain.Usuario;
 import br.ufes.informatica.jurisproc.core.persistence.UsuarioDAO;
@@ -25,22 +27,26 @@ public class CurrentUser implements Serializable
 	@Inject
 	private UsuarioDAO usuarioDAO;
 	private Usuario usuario;
-	
+
 	public Usuario getUsuario()
 	{
 		return usuario;
 	}
-	
+
 	@PostConstruct
 	private void carregaUsuario()
 	{
 		Principal principal = request.getUserPrincipal();
-		if ( principal != null)
+		if (principal != null)
 		{
 			this.usuario = usuarioDAO.buscaPorEmail(principal.getName());
 		}
 	}
-	
-	
+
+	public String logout()
+	{
+		((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false)).invalidate();
+		return "/users/login.xhtml?faces-redirect=true";
+	}
 
 }
