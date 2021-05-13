e0b3540cb2b3da15065a6460e3bf6e71b1926510
Pedro Hoppe <pedrohbh@github.com>
2021-05-12 21:08:49 -0300

    Finalizando consumo de dados

--- jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/controller/MapaViewController.java
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/controller/MapaViewController.java
@@ -60,45 +60,6 @@ public class MapaViewController
 
 	}
 
-//	private void testeMontagem()
-//	{
-//		simpleModel = new DefaultMapModel();
-//
-//		LatLng coord1 = new LatLng(36.879466, 30.667648);
-//		LatLng coord2 = new LatLng(36.883707, 30.689216);
-//		LatLng coord3 = new LatLng(36.879703, 30.706707);
-//		LatLng coord4 = new LatLng(36.885233, 30.702323);
-//
-//		simpleModel.addOverlay(new Marker(coord1, "Konyaalti"));
-//		simpleModel.addOverlay(new Marker(coord2, "Ataturk Parki"));
-//		simpleModel.addOverlay(new Marker(coord3, "Karaalioglu Parki"));
-//		simpleModel.addOverlay(new Marker(coord4, "Kaleici"));
-//	}
-
-	/*
-	 * private void adicionaNoMapaTeste() { String nome = "Cariacica"; if ( nome !=
-	 * null && nome.length() > 3) { String query =
-	 * "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
-	 * "PREFIX dbp: <http://dbpedia.org/property/>\n" +
-	 * "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + "SELECT ?desc\n" +
-	 * "WHERE {\n" + " ?uri a dbo:City ;\n" + "dbp:name \"Rio de Janeiro\"@en ;\n" +
-	 * "rdfs:comment ?desc .\n" + "FILTER (langMatches(lang(?desc), \"EN\"))\n" +
-	 * "}";
-	 * 
-	 * QueryExecution queryExecution =
-	 * QueryExecutionFactory.sparqlService("https://dbpedia.org/sparql", query);
-	 * 
-	 * ResultSet results = queryExecution.execSelect();
-	 * 
-	 * if ( results.hasNext() ) { QuerySolution querySolution = results.next();
-	 * Literal literal = querySolution.getLiteral("desc");
-	 * System.out.println(literal.getValue()); }
-	 * 
-	 * }
-	 * 
-	 * }
-	 */
-
 	public MapModel getSimpleModel()
 	{
 		return simpleModel;
