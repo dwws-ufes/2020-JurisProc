6d32821010572251d28c07fabad453454df4050a
Pedro Hoppe <pedrohbh@github.com>
2021-05-12 21:04:09 -0300

    Comentário

--- jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/controller/MapaViewController.java
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/core/controller/MapaViewController.java
@@ -59,20 +59,20 @@ public class MapaViewController
 
 	}
 
-	private void testeMontagem()
-	{
-		simpleModel = new DefaultMapModel();
-
-		LatLng coord1 = new LatLng(36.879466, 30.667648);
-		LatLng coord2 = new LatLng(36.883707, 30.689216);
-		LatLng coord3 = new LatLng(36.879703, 30.706707);
-		LatLng coord4 = new LatLng(36.885233, 30.702323);
-
-		simpleModel.addOverlay(new Marker(coord1, "Konyaalti"));
-		simpleModel.addOverlay(new Marker(coord2, "Ataturk Parki"));
-		simpleModel.addOverlay(new Marker(coord3, "Karaalioglu Parki"));
-		simpleModel.addOverlay(new Marker(coord4, "Kaleici"));
-	}
+//	private void testeMontagem()
+//	{
+//		simpleModel = new DefaultMapModel();
+//
+//		LatLng coord1 = new LatLng(36.879466, 30.667648);
+//		LatLng coord2 = new LatLng(36.883707, 30.689216);
+//		LatLng coord3 = new LatLng(36.879703, 30.706707);
+//		LatLng coord4 = new LatLng(36.885233, 30.702323);
+//
+//		simpleModel.addOverlay(new Marker(coord1, "Konyaalti"));
+//		simpleModel.addOverlay(new Marker(coord2, "Ataturk Parki"));
+//		simpleModel.addOverlay(new Marker(coord3, "Karaalioglu Parki"));
+//		simpleModel.addOverlay(new Marker(coord4, "Kaleici"));
+//	}
 
 	/*
 	 * private void adicionaNoMapaTeste() { String nome = "Cariacica"; if ( nome !=
