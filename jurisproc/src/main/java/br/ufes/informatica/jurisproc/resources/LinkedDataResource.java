0ba7a77413f191437eaa5b0c00bdf493a4796481
Pedro Hoppe <pedrohbh@github.com>
2021-05-15 19:10:00 -0300

    Adição de serviço de consulta - parte 2

--- jurisproc/src/main/java/br/ufes/informatica/jurisproc/resources/LinkedDataResource.java
+++ jurisproc/src/main/java/br/ufes/informatica/jurisproc/resources/LinkedDataResource.java
@@ -4,6 +4,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,8 +18,12 @@ public class LinkedDataResource
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public Response executarConsulta(@FormParam("query") String query)
 	{
-		System.out.println(query);
-		return null;
+		Client client = ClientBuilder.newClient();
+		String uri = "http://localhost:2020/sparql";
+		Form dadosDeEntradaForm = new Form("query", query);
+		Entity<Form> formEntity = Entity.form(dadosDeEntradaForm);
+		Response response = client.target(uri).request().post(formEntity);
+		return response;		
 	}
 
 }
